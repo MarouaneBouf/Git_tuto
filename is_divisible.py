ae70ce1e47fd3494a2588ba6fa0279ede961f8bd
MarouaneBouf <marouanebouf3@gmail.com>
2022-09-04 14:12:45 +0100

    Comments added

--- is_divisible.py
+++ is_divisible.py
@@ -10,4 +10,10 @@ def is_divisible(n,x,y):
 
 #Check if prime
 def is_prime(n):
+    """
+    Return True if all the numbers from 2 to n-1 are not divisors of n.
+    
+    :param n: the number to check
+    :return: True or False
+    """
     return all(n%i for i in range(2,n))
