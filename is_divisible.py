a44e9d8274097efde60aa0cbd904456c9de253f2
MarouaneBouf <104838272+MarouaneBouf@users.noreply.github.com>
2022-09-04 13:41:03 +0100

    Add main function and comments

--- is_divisible.py
+++ is_divisible.py
@@ -1,6 +1,13 @@
+
+# main function
+def main():
+    number = 7
+    print(is_prime(number))
+
 # Is n divisible by x and y?
 def is_divisible(n,x,y):
     return n % x == 0 and n % y == 0
 
+#Check if prime
 def is_prime(n):
     return all(n%i for i in range(2,n))
